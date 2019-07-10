e07c326f982e7fbace0264fe3b75a4cc0dfc639e
Dima Gradovsky <grad3r@ex.ua>
2019-07-10 13:01:53 +0000

    fixed bugs

--- js/index.js
+++ js/index.js
@@ -33,20 +33,3 @@ function getFetchJSON () {
         document.querySelector('.doctor-activity').innerHTML = doctorsData.activity;
     });
 }
-
-const text = document.querySelector('textarea');
-
-document.querySelector('input[type=submit]')
-  .addEventListener('click', saveText);
-
-document.querySelector('.load')
-  .addEventListener('click', loadText);
-
-function saveText(e) {
-  e.preventDefault();
-  localStorage['text'] = text.value;
-}
-
-function loadText() {
-  text.value = localStorage['text'] || '';
-}
